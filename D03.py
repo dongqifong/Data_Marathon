7b7ad91436c7619235c11b8f6f75928a0b888d9a
Henry D <qifong04@gmail.com>
2021-01-18 13:01:57 +0800

    Day 3_v2

--- D03.py
+++ D03.py
@@ -8,12 +8,14 @@ else:
 #1
 V1=20000
 V0=20
-db=10*np.log10(V1)/np.log(V0)#<---------anser
+db=20*np.log10(V1/V0)#<---------anser
 print(db)
 
 #2
-#db=10log10(V)
-#V=10^(db/10)
-#V2/V1=10^(db2/10-db1/10)
+#db=20log10(V/V0)
+#V=10^(db/20)*V0
+#V2/V1=10^((db2/20-db1/20)*V0)
+db1=30
+db2=50
 ratio=np.power(10,(db2-db1)/10)#<--------anser
 print(ratio)
